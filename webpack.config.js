d73f1a83c9626cc0919c3dbb07bb6551ea409e00
Nathan Chau <nateychau@gmail.com>
2021-01-02 14:05:39 -0800

    Modify post install scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: ['babel-polyfill', './src/index.jsx'],
-    output: {
-        path: path.resolve(__dirname),
-        filename: './bundle.js'
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: [/\.jsx?$/],
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    query: {
-                        presets: ['@babel/env', '@babel/react']
-                    }
-                },
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".js", '.jsx', '*']
-    }
+  entry: ["babel-polyfill", "./src/index.jsx"],
+  output: {
+    path: path.resolve(__dirname),
+    filename: "./bundle.js",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/env", "@babel/react"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", "*"],
+  },
+  watch: process.env.NODE_ENV !== "production" ? true : false,
+  watchOptions: { poll: process.env.NODE_ENV !== "production" ? true : false },
 };
